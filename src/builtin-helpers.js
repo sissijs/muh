0b9947b9530abec700670e5490f76d3ca529f028
Lea Rosema <lea@lea.codes>
2024-12-31 16:00:14 +0100

    fix: update helpers to also include options, add jsdoc

--- src/builtin-helpers.js
+++ src/builtin-helpers.js
@@ -1,9 +1,21 @@
-export async function fetchJson(request) {
-  const response = await fetch(request);
+/**
+ * Fetch JSON. Small wrapper around native fetch
+ * @param {string|URL} request 
+ * @param {RequestInit} options 
+ * @returns json response
+ */
+export async function fetchJson(request, options) {
+  const response = await fetch(request, options);
   return await response.json();
 }
 
-export async function fetchText(request) {
-  const response = await fetch(request);
+/**
+ * Fetch Text. Small wrapper around native fetch
+ * @param {string|URL} request 
+ * @param {RequestInit} options RequestInfo
+ * @returns 
+ */
+export async function fetchText(request, options) {
+  const response = await fetch(request, options);
   return await response.text();
 }
